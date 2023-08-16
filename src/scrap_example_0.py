1455e4be669a4e04d0595e5487de3053e7da7066
kvktran2812 <kvktran@gmail.com>
2023-08-15 22:55:23 -0400

    scrape function graph runs

--- src/scrap_example_0.py
+++ src/scrap_example_0.py
@@ -2,23 +2,17 @@ from src.machine import *
 import re
 import networkx as nx
 import matplotlib.pyplot as plt
+from src.scraping import Scraping
 
 
-def func1():
-    print("Hello World")
+def func():
+    print("hello")
 
 
-graph = nx.MultiDiGraph()
-graph.add_node("New York")
-graph.add_node("Tokyo")
-graph.add_node("London")
-graph.add_node("Dehli")
-graph.add_edge("New York", "Tokyo")
-graph.add_edge("New York", "Dehli")
-graph.add_edge("Tokyo", "London")
+driver = standard_driver()
 
-London = graph.nodes["London"]
-London["function"] = func1
-print(London)
-
-print(graph.out_edges("New York"))
+scraping = Scraping(driver)
+scraping.add_step(1, func)
+scraping.add_step(2, func)
+scraping.add_step(3, func, 1)
+scraping.run()
--- src/scraping.py
+++ src/scraping.py
@@ -15,16 +15,35 @@ class Scraping:
         return
 
     def run(self):
+        # Get next nodes from Root node
+        edges = self._graph.out_edges("Root")
+        next_nodes = [edge[1] for edge in edges]
+        current_nodes = []
+
+        while next_nodes:
+            current_nodes = next_nodes
+            next_nodes = []
+            for node in current_nodes:
+                node_data = self._graph.nodes[node]
+                print(node, node_data)
+
+                # Get to next node
+                edges = self._graph.out_edges(node)
+                [next_nodes.append(edge[-1]) for edge in edges]
+
         return
 
     def add_step(self, node_name, function, prev_step=None):
         self._graph.add_node(node_name)
-        self._graph[node_name]['function'] = function
+        self._graph.nodes[node_name]['function'] = function
 
         if prev_step:
             self._graph.add_edge(prev_step, node_name)
         else:
             self._graph.add_edge("Root", node_name)
+
+    def _validate_function(self):
+        return
 
 
 class Step:
