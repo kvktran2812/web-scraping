d88aaa78c8dc5aaf725962443903cb3305068638
kvktran2812 <kvktran@gmail.com>
2023-08-17 12:12:13 -0400

    general changes in google map scraping

--- src/machine.py
+++ src/machine.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 import re
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def standard_driver():
@@ -17,27 +19,24 @@ def standard_driver():
 
 
 class GoogleMapMachine:
-    def __init__(self, name, driver=None):
+    def __init__(self, driver=None):
         if driver:
             self.driver = driver
         else:
             self.driver = standard_driver()
-
-        self.name = name
         self.elements = {}
         self.steps = []
         self.data = {}
 
-    def _setup_resource(self):
+    def _setup_resource(self, name):
         self.driver.get("https://www.google.com/maps")
         self.elements['search_input'] = '//*[@id="searchboxinput"]'
         elem = self.driver.find_element(By.XPATH, '//*[@id="searchboxinput"]')
-        elem.send_keys("Sushi Moto, Yonge Street, Toronto, ON")
+        elem.send_keys(name)
         elem.send_keys(Keys.ENTER)
-        time.sleep(2)
-        return
+        time.sleep(1.5)
 
-    def get_general_info(self):
+    def _get_general_info(self):
         info_xpath = '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div'
         info = self.driver.find_element(By.XPATH, info_xpath)
         html = info.get_attribute("innerHTML")
@@ -56,16 +55,22 @@ class GoogleMapMachine:
             'number_of_review': int(number_of_review),
             'reviews': [],
         }
+        time.sleep(1.5)
 
-    def run(self):
-        self._setup_resource()
-        self.get_general_info()
+    def _go_to_reviews_section(self):
+        reviews_xpath = '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]'
+        reviews_button = self.driver.find_element(By.XPATH, reviews_xpath)
+        reviews_button.send_keys(Keys.ENTER)
+        time.sleep(1.5)
 
+        elem = self.driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]')
+        html = elem.get_attribute("innerHTML")
 
-class ScrapeStep:
-    def __init__(self, driver):
-        self.driver = driver
-        return
+        # page_source = self.driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        print(soup.prettify())
 
-    def run(self):
-        return
+    def scrape(self, name):
+        self._setup_resource(name)
+        self._get_general_info()
+        self._go_to_reviews_section()
--- src/scrap_example_0.py
+++ src/scrap_example_0.py
@@ -1,4 +1,7 @@
 from src.machine import *
 from src.scraping import *
 
+gmap = GoogleMapMachine()
+gmap.scrape("Sushi Moto")
 
+print(gmap.data)
